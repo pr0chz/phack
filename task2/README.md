6011513b40ab800f211702c23b8bc085639cbf47
pr0chy <pystole@gmail.com>
2017-04-09 16:21:36 +0200

    Task 2

---
+++ task2/README.md
@@ -0,0 +1,13 @@
+## Usage
+
+Prepare environment
+```
+virtualenv venv
+$ source venv/bin/activate
+venv$ pip install -r requirements.pip
+```
+
+Run
+```
+cat input | python task2.py
+```
---
+++ task2/task2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import json
+import sys
+
+input = json.loads(sys.stdin.read())
+
+m = pd.DataFrame(input['measurements'])
+s = pd.DataFrame(input['samples'])
+
+fields = ['brake-distance', 'noise-level', 'vibrations']
+
+model = LogisticRegression()
+model.fit(m[fields], m['type'])
+
+s['type'] = model.predict(s[fields])
+
+result = s[['id', 'type']].to_dict(orient="records")
+result = json.dumps({"result": result})
+
+print(result)
